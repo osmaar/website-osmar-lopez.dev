2dd033c036c2e7ad5ea408801e422f71b10c236a
dev.generico7 <dev.generico7@gmail.com>
2025-07-11 15:27:29 -0600

    fix: actualizar el nombre del proyecto a "osmar lopez" en archivos de configuración

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.sanju.sh",
+	site: "https://www.osmar-lopez.dev",
 	integrations: [
 		mdx(),
 		sitemap({

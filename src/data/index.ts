96b20bfef3e7ae4d225fe4df6ca59fa3d26e10f7
dev.generico7 <dev.generico7@gmail.com>
2025-07-11 15:27:47 -0600

    refactor: actualizar el contenido y la estructura del archivo index.ts para reflejar la información de Osmar López

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,84 +1,81 @@
-export const SITE_TITLE = "Sanju's Space";
-export const SITE_DESCRIPTION = "A internet space for Sanju.";
+export const SITE_TITLE = 'Espacio de Osmar López';
+export const SITE_DESCRIPTION = 'Osmar López desarrollador full stack.';
 
 export interface MenuItem {
-	label: string;
-	url: string;
+  label: string;
+  url?: string;
+  external?: boolean;
 }
 
 export const menuItems: MenuItem[] = [
-	{
-		label: "sanju",
-		url: "/",
-	},
-	{
-		label: "writings",
-		url: "/writings",
-	},
-	{
-		label: "thoughts",
-		url: "/thoughts",
-	},
-	{
-		label: "ships",
-		url: "/ships",
-	},
+  {
+    label: 'osmi-dev',
+    url: '/',
+  },
+  {
+    label: 'portafolio',
+    url: 'https://cv.osmar-lopez.dev',
+    external: true,
+  },
+  {
+    label: 'freelance',
+    url: '/freelance',
+  },
 ];
 
-export const title = "Sanju's Space";
-export const description = "A internet space for Sanju.";
-export const image = "/images/ogimage.png";
-export const url = "https://sanju.sh";
+export const title = 'Espacio de Osmar López';
+export const description = 'Osmar López desarrollador full stack.';
+export const image = '/images/ogimage.png';
+export const url = 'https://osmar-lopez.dev';
 
 export const ogImage = {
-	src: "/images/ogimage.png",
-	alt: "Sanju's Space",
+  src: '/images/ogimage.png',
+  alt: 'Osmar Space',
 };
 
-
 export const products = [
-	{
-		name: "Fli.so",
-		url: "https://fli.so",
-		image: "/products/fli.png",
-	},
-	{
-		name: "Uiino",
-		url: "https://uiino.com",
-		image: "/products/uiino.png",
-	},
-	{
-		name: "SticAI",
-		url: "https://sticai.com",
-		image: "/products/sticai.png",
-	},
-	{
-		name: "Dun",
-		url: "https://dunsuite.com",
-		image: "/products/dun.png",
-	},
-	{
-		name: "DunTasks",
-		url: "https://duntasks.com",
-		image: "/products/duntasks.png",
-	},
+  {
+    name: 'Fli.so',
+    url: 'https://fli.so',
+    image: '/products/fli.png',
+  },
+  {
+    name: 'Uiino',
+    url: 'https://uiino.com',
+    image: '/products/uiino.png',
+  },
+  {
+    name: 'SticAI',
+    url: 'https://sticai.com',
+    image: '/products/sticai.png',
+  },
+  {
+    name: 'Dun',
+    url: 'https://dunsuite.com',
+    image: '/products/dun.png',
+  },
+  {
+    name: 'DunTasks',
+    url: 'https://duntasks.com',
+    image: '/products/duntasks.png',
+  },
 ];
 
 export const socialLinks = [
-	{
-		label: "@x",
-		url: "https://x.com/spikeysanju",
-	},
-	{
-		label: "email",
-		url: "mailto:work@sanju.sh",
-	},
-	{
-		label: "github",
-		url: "https://github.com/spikeysanju",
-	},
-	{
-		label: "linkedin",
-		url: "https://www.linkedin.com/in/imsanju",
-	},
+  {
+    label: 'email',
+    url: 'mailto:contacto@osmar-lopez.dev',
+  },
+  {
+    label: 'YouTube',
+    url: 'https://www.youtube.com/@Est-Funny',
+  },
+  {
+    label: 'github',
+    url: 'https://github.com/osmaar',
+  },
+  {
+    label: 'linkedin',
+    url: 'https://www.linkedin.com/in/osmi-developer/',
+  },
 ];
